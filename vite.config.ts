317376e593d6627dd1b271d94f4fe8d535c2d053
EZ-Grades <ezgrades.business@gmail.com>
2025-09-26 21:38:03 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/Final-App/',
+    base: '/easy-grades/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
